32cbba93b5db5526068319299bfa887be1f5f198
rtibell <rasmus.tibell@gmail.com>
2024-10-02 21:56:03 +0200

    first commit

---
+++ src/main/java/com/tibell/spring/localnews2/Localnews2Application.java
@@ -0,0 +1,13 @@
+package com.tibell.spring.localnews2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Localnews2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Localnews2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tibell/spring/localnews2/Localnews2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tibell.spring.localnews2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Localnews2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
